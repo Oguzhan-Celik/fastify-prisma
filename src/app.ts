f776f3be4fd47282a4e7bdaf851e8392f53d9112
Your Name <oguz75364@gmail.com>
2022-08-19 17:51:35 +0300

    restapi-fastify

---
+++ src/app.ts
@@ -0,0 +1,49 @@
+import Fastify, { FastifyReply, FastifyRequest } from "fastify";
+import fjwt from "@fastify/jwt";
+import userRoutes from "./modules/user/user.route";
+import { userSchemas } from "./modules/user/user.schema";
+
+export const server = Fastify();
+
+declare module "fastify" {
+  export interface FastifyInstance {
+    authenticate: any;
+  }
+}
+
+server.register(fjwt, {
+  secret: "bababuy423sgasclı5afclşdv992bjsju31kuığüişçöç",
+});
+
+server.decorate(
+  "authenticate",
+  async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      await request.jwtVerify();
+    } catch (e) {
+      return reply.send(e);
+    }
+  }
+);
+
+server.get("/healthcheck", async function () {
+  return { status: "OK" };
+});
+
+async function main() {
+  for (const schema of userSchemas) {
+    server.addSchema(schema);
+  }
+
+  server.register(userRoutes, { prefix: "api/users" });
+
+  try {
+    await server.listen(3000, "0.0.0.0");
+    console.log(`Server ready at http://localhost:3000`);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+main();
